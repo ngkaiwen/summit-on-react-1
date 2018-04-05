c615aa389ec5c3d1fc3bb06e9a47423806dc7009
Candice Tay <candicetayjh@Hotmail.sg>
2018-04-05 18:49:18 +0800

    Added Student Code Combat Charts
    Added 2 Code Combat related charts for students
    
    - Breakdown of time spent and comparison with similar users
    - Simple statistics of achievements (bare)

---
+++ src/Dashboard/ChartSpace/Students/StudentDetails/StudentDetailsPanel/StudentCodeCombatCharts/TimeBreakDownChart.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { PieChart,Pie,Cell,Tooltip,ResponsiveContainer,Legend } from "recharts";
+
+class TimeBreakDownChart extends Component {
+  render() {
+    const pieData = this.props.pieData /*array*/
+    if (pieData["ownChart"] == null) { return null }
+    const colors = ["#AED581","#F06292"]
+
+    return (
+      <div className = "chart">
+        <ResponsiveContainer width="100%" height = {200}>
+          <PieChart>
+            <Legend verticalAlign="bottom" height={15}/>
+            <Pie data = {pieData["ownChart"]}
+              dataKey="Value"
+              nameKey="Name"
+              innerRadius={1}
+              outerRadius={80}
+              paddingAngle = {1}>
+              { pieData["ownChart"].map( (entry,index) => (<Cell fill = {colors[index]} key = {index}/>)) }
+            </Pie>
+            <Tooltip/>
+          </PieChart>
+        </ResponsiveContainer>
+      </div>);
+    }
+  }
+
+  export default TimeBreakDownChart;
---
+++ src/Dashboard/ChartSpace/Students/StudentDetails/StudentDetailsPanel/StudentCodeCombatCharts/TimeBreakDownComparisonChart.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { PieChart,Pie,Cell,Tooltip,ResponsiveContainer,Legend } from "recharts";
+
+class TimeBreakDownComparisonChart extends Component {
+  render() {
+    const pieData = this.props.pieData /*array*/
+    if (pieData["comparisonChart"] == null) { return null }
+    const colors = ["#AED581","#F06292"]
+
+    return (
+      <div className = "chart">
+      <ResponsiveContainer width="100%" height = {200}>
+      <PieChart>
+      <Legend verticalAlign="bottom" height={15}/>
+      <Pie data = {pieData["comparisonChart"]}
+      dataKey="Value"
+      nameKey="Name"
+      innerRadius={40}
+      outerRadius={80}
+      paddingAngle = {1}>
+      { pieData["comparisonChart"].map( (entry,index) => (<Cell fill = {colors[index]} key = {index}/>)) }
+      </Pie>
+      <Tooltip/>
+      </PieChart>
+      </ResponsiveContainer>
+      </div>
+    );
+    }
+  }
+
+  export default TimeBreakDownComparisonChart;
