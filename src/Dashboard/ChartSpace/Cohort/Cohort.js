f62dfc5a32f63482d81b2c2501e7ab70523e3d58
Ian Sim <theiansim@gmail.com>
2018-04-18 23:04:04 +0800

    added cohort scaffolding

--- src/Dashboard/ChartSpace/Cohort/Cohort.js
+++ src/Dashboard/ChartSpace/Cohort/Cohort.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Cohort extends Component {
 
   render() {
-    console.log("this is cohort")
+    const CCdata = this.props.data;
+    
     return (
-        null
+      CCdata ?
+      <div className='cohort-main-container'>
+          <h1>Cohort Comparison</h1>
+          <div className='cohort-grid-container'>
+
+            <div className='cohort-grid-item-blank'>
+                <h2>All courses</h2>
+            </div>
+
+            <div className='cohort-grid-item'>
+                <h2>All courses</h2>
+            </div>
+
+            <div className='cohort-grid-item'>
+                <h2>All courses</h2>
+            </div>
+
+            <div className='cohort-grid-item'>
+                <h2>All courses</h2>
+            </div>
+
+            <div className='cohort-grid-item-joint'>
+                <div className='cohort-grid-item-joint-inner'> 
+                  <h2>internal</h2>
+                </div>
+
+                <div className='cohort-grid-item-joint-inner'> 
+                  <h2>internal</h2>
+                </div>
+
+                <div className='cohort-grid-item-joint-inner'> 
+                  <h2>internal</h2>
+                </div>
+
+                <div className='cohort-grid-item-joint-inner'> 
+                  <h2>internal</h2>
+                </div>
+            </div>
+
+            <div className='cohort-grid-item-joint'>
+                <h2>All courses</h2>
+            </div>
+             
+          </div>
+      </div> : null
     );
   }
 }
 
-export default Cohort;
+function mapStateToProps(state){
+  return {
+    data: state["cc_data"]
+  };
+};
+
+export default connect(mapStateToProps)(Cohort);
