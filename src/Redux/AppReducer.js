1baec823c420280e1cf2b4fd55093d126cfbbabf
suenwl <suenwailun@gmail.com>
2018-04-20 00:25:55 +0800

    update firebase part 2

--- src/Redux/AppReducer.js
+++ src/Redux/AppReducer.js
@@ -23,9 +23,9 @@ function dataFilter(id,data) {
 }
 
 const mapping = {
-	'RfSxvvECevRxl85KxWnjwfUt4LQ2': ['admin','ADMINISTRATOR'], //admins
-	'Xml7fH5dREWgRAIFGP53Qv1YXH92':['R6nSbDVly8PUnC6jQFcseDS9sgJ3','EDUCATOR'], //boesch
-	"sQD0Ksct99UZSLrsMlxkUSaGu3E3" : ['Ab947q6H2eQ1DjJzhu4GwQXQ9vz1','EDUCATOR'] //venu
+	'koThLjSzltZLjZsBSohCCaQjpW53': ['admin','ADMINISTRATOR'], //admins
+	'akD92Zz4hONCPibCqC48furBPmz1':['R6nSbDVly8PUnC6jQFcseDS9sgJ3','EDUCATOR'], //boesch
+	"2DRYtqS33yeiFHyNS8HWALNMxQh1" : ['Ab947q6H2eQ1DjJzhu4GwQXQ9vz1','EDUCATOR'] //venu
 }
 
 //define a reducer with an initialized state action
