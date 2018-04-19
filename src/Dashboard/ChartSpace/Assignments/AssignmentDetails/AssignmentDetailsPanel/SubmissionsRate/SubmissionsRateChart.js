ccb0c719559bdb266d8560d204c1d1a2fe4ff2dc
ngkaiwen <ng-kai-wen@outlook.com>
2018-04-19 20:38:35 +0800

    Finished Courses tab

--- src/Dashboard/ChartSpace/Assignments/AssignmentDetails/AssignmentDetailsPanel/SubmissionsRate/SubmissionsRateChart.js
+++ src/Dashboard/ChartSpace/Assignments/AssignmentDetails/AssignmentDetailsPanel/SubmissionsRate/SubmissionsRateChart.js
@@ -3,8 +3,9 @@ import {LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, Label} from
 
 const SubmissionsRateChart = (props) => {
     //console.log(props.selectedData)
-    return (
-        <ResponsiveContainer width="100%" height = {400}>
+      return ( props.selectedData ?
+        <div>
+          <h4 className="assignments-title">Submissions Rate </h4>        <ResponsiveContainer width="100%" height = {400}>
             <LineChart width={800} height={300} data={props.selectedData} margin={{ top: 10, right: 20, left: 10, bottom: 20 }}>
                 <XAxis dataKey="Date">
                     <Label value="Time" offset={0} position="bottom" />
@@ -16,6 +17,7 @@ const SubmissionsRateChart = (props) => {
                 <Line type="monotone" dataKey="Students" stroke="#82ca9d" activeDot={{r: 8}}/>
             </LineChart>
         </ResponsiveContainer>
+        </div> : null
     );
 };
 
--- src/Dashboard/ChartSpace/Courses/Courses.js
+++ src/Dashboard/ChartSpace/Courses/Courses.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import BarChartTimePerLevel from './Charts/BarChartTimePerLevel'
 import BarChartTimePerStudent from './Charts/BarChartTimePerStudent'
-import BarChartLevelPerStudent from './Charts/BarChartLevelPerStudent'
+import LineChartLevelPerStudent from './Charts/LineChartLevelPerStudent'
+import LineChartLevels75 from './Charts/LineChartLevels75'
 
 class Courses extends Component {
 
@@ -16,14 +17,14 @@ class Courses extends Component {
           <div className='courses-grid-container'>
 
           <div className='course-grid-item-joint'>
-              <h2>Average Time per Level<h5> for each Course (in Seconds)</h5></h2>
+              <h2>Distribution of average time spent per level<h5> for students in each Course (Minutes)</h5></h2>
               <BarChartTimePerLevel data = {CCdata["AverageTimePerLevel"]}/>
           </div>
               <div className='course-grid-item'>
                   <h4>Summary Statistics For Time Spent</h4>
                   <br/>
                   <br/>
-                  <p><b>Median School: </b>{CCdata["AverageTimePerLevelSummaryStats"]["AverageData"]["AverageSchool"]}, {CCdata["AverageTimePerStudentSummaryStats"]["AverageData"]["Timing"]} Sec</p>
+                  <p><b>Median School: </b>{CCdata["AverageTimePerLevelSummaryStats"]["AverageData"]["AverageSchool"]}, {CCdata["AverageTimePerStudentSummaryStats"]["AverageData"]["Timing"]} Min</p>
                   <br/>
                   <p><b>Standard Deviation: </b>{CCdata["AverageTimePerLevelSummaryStats"]["StandardDeviationStats"]["Standard Deviation"]}</p>
               </div>
@@ -31,14 +32,14 @@ class Courses extends Component {
 
           <div className='courses-grid-container'>
           <div className='course-grid-item-joint'>
-              <h2>Average Time per Student<h5> for each Course (in Seconds)</h5></h2>
+              <h2>Distribution of average time spent<h5> for students in each Course (Minutes)</h5></h2>
               <BarChartTimePerStudent data = {CCdata["AverageTimePerStudent"]}/>
           </div>
               <div className='course-grid-item'>
                   <h4>Summary Statistics For Time Spent</h4>
                   <br/>
                   <br/>
-                  <p><b>Median School: </b>{CCdata["AverageTimePerStudentSummaryStats"]["AverageData"]["AverageSchool"]}, {CCdata["AverageTimePerStudentSummaryStats"]["AverageData"]["Timing"]} Sec</p>
+                  <p><b>Median School: </b>{CCdata["AverageTimePerStudentSummaryStats"]["AverageData"]["AverageSchool"]}, {CCdata["AverageTimePerStudentSummaryStats"]["AverageData"]["Timing"]} Min</p>
                   <br/>
                   <p><b>Standard Deviation: </b>{CCdata["AverageTimePerStudentSummaryStats"]["StandardDeviationStats"]["Standard Deviation"]}</p>
               </div>
@@ -46,17 +47,32 @@ class Courses extends Component {
 
           <div className='courses-grid-container'>
           <div className='course-grid-item-joint'>
-              <h2>Average Level per Student<h5> for each Course</h5></h2>
-              <BarChartLevelPerStudent data = {CCdata["AverageLevelPerStudent"]}/>
+              <h2>Distribution of average levels completed<h5> for students in each course</h5></h2>
+              <LineChartLevelPerStudent data = {CCdata["AverageLevelPerStudent"]}/>
           </div>
               <div className='course-grid-item'>
                   <h4>Summary Statistics</h4>
                   <br/>
                   <br/>
-                  <p><b>Median School: </b>{CCdata["AverageLevelPerStudentSummaryStats"]["AverageData"]["AverageSchool"]}, {CCdata["AverageLevelPerStudentSummaryStats"]["AverageData"]["Timing"]} Sec</p>
+                  <p><b>Median School: </b>{CCdata["AverageLevelPerStudentSummaryStats"]["AverageData"]["AverageSchool"]}, {CCdata["AverageLevelPerStudentSummaryStats"]["AverageData"]["Levels"]}</p>
                   <br/>
                   <p><b>Standard Deviation: </b>{CCdata["AverageLevelPerStudentSummaryStats"]["StandardDeviationStats"]["Standard Deviation"]}</p>
               </div>
+          </div>
+
+          <div className='courses-grid-container'>
+          <div className='course-grid-item-joint'>
+              <h2>Maximum Number of Levels Completed (>=75% Completion Rate)<h5> for each Course</h5></h2>
+              <LineChartLevels75 data = {CCdata["MaxLevels75Course"]}/>
+          </div>
+          <div className='course-grid-item'>
+              <h4>Summary Statistics</h4>
+              <br/>
+              <br/>
+              <p><b>Median School: </b>{CCdata["MaxLevels75CourseSummaryStats"]["AverageData"]["AverageSchool"]}, {CCdata["AverageLevelPerStudentSummaryStats"]["AverageData"]["Levels"]}</p>
+              <br/>
+              <p><b>Standard Deviation: </b>{CCdata["MaxLevels75CourseSummaryStats"]["StandardDeviationStats"]["Standard Deviation"]}</p>
+          </div>
           </div>
 
       </div> : null
